ea73a38dd730172be7e350fd48fe3302f993eb8f
waynemcfetridge <wayne@weareva.com.au>
2020-05-12 22:35:43 +0000

    Update from Forestry.io nik@thevariousartists.com.au updated terms.md

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -40,6 +40,12 @@ $(function() {
   });
 
 
+  $('.row-work .work-teaser').matchHeight();
+
+
+//  restart.addEventListener("click", () => logoAnimation.restart());
+
+
 
   // Wrap every letter in a span
   // var textWrapper = document.querySelector('.animate .create');
