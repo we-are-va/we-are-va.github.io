5dbc761a1703debb82e33b6195a04da9f556f386
waynemcfetridge <wayne@weareva.com.au>
2020-05-12 01:25:34 +0000

    front updates

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -40,6 +40,42 @@ $(function() {
   });
 
 
+
+  // Wrap every letter in a span
+  // var textWrapper = document.querySelector('.animate .create');
+  // textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+  // anime.timeline({loop: true})
+  //   // .add({
+  //   //   targets: '.ml11 .line',
+  //   //   scaleY: [0,1],
+  //   //   opacity: [0.5,1],
+  //   //   easing: "easeOutExpo",
+  //   //   duration: 700
+  //   // })
+  //   // .add({
+  //   //   targets: '.ml11 .line',
+  //   //   translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+  //   //   easing: "easeOutExpo",
+  //   //   duration: 700,
+  //   //   delay: 100
+  //   // })
+  //   .add({
+  //     targets: '.animate .letter',
+  //     opacity: [0,1],
+  //     easing: "easeOutExpo",
+  //     duration: 600,
+  //     offset: '-=775',
+  //     delay: function(el, i) { 34 * (i+1) }
+  //   }).add({
+  //     targets: '.animate',
+  //     opacity: 0,
+  //     duration: 1000,
+  //     easing: "easeOutExpo",
+  //     delay: 1000
+  //   });
+
+  // Image Comparison
   document.querySelectorAll('.cocoen').forEach(function(element){
     new Cocoen(element);
   });
@@ -128,7 +164,7 @@ $(function() {
   //       smoothState = $page.smoothState(options).data('smoothState');
   // });
 
-  console.log("Welcome to VA BOOOOOOM!!!");
+  console.log("Welcome to WE ARE VA");
   // console.log("##:::::'##:'########::::'###::::'########::'########:'##::::'##::::'###::::");
   // console.log("##:'##: ##: ##.....::::'## ##::: ##.... ##: ##.....:: ##:::: ##:::'## ##:::");
   // console.log("##: ##: ##: ##::::::::'##:. ##:: ##:::: ##: ##::::::: ##:::: ##::'##:. ##::");
