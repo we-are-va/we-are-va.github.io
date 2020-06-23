8a1398ab928e6e2f261c0d56f8f958cabed44e63
waynemcfetridge <wayne@weareva.com.au>
2020-06-23 02:04:36 +0000

    style updates

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -56,13 +56,25 @@ $(function() {
 
   
 
-  $('.row-work .work-teaser').matchHeight();
-
 
   $( document ).ready(function() {
-  // Image Comparison
-    document.querySelectorAll('.cocoen').forEach(function(element){
-      new Cocoen(element);
+  
+    // document.querySelectorAll('.cocoen').forEach(function(element){
+    //   new Cocoen(element);
+    // });
+
+    
+
+    // Image Comparison
+    $(".twentytwenty-container").twentytwenty({
+      // default_offset_pct: 0.7, // How much of the before image is visible when the page loads
+      // orientation: 'horizontal', // Orientation of the before and after images ('horizontal' or 'vertical')
+      // before_label: 'Before', // Set a custom before label
+      // after_label: 'After', // Set a custom after label
+      // no_overlay: false, //Do not show the overlay with before and after
+      // move_slider_on_hover: true, // Move slider on mouse hover?
+      // move_with_handle_only: true, // Allow a user to swipe anywhere on the image to control slider movement. 
+      // click_to_move: false // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
     });
 
   });
